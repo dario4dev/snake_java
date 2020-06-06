03197b341dafc86e5da39cf88de63db41ecc1d5b
Dario Zampini <>
2020-06-06 15:46:32 +0200

    Added new game kit support

---
+++ src/SnakeGame.java
@@ -0,0 +1,26 @@
+public class SnakeGame extends Game {
+
+    public SnakeGame(IGameWindowProperties windowProperties) {
+        super(windowProperties);
+    }
+
+    @Override
+    public void InitialiseSystems() {
+
+    }
+
+    @Override
+    public void DeinitialiseSystems() {
+
+    }
+
+    @Override
+    public void InitialiseComponents() {
+
+    }
+
+    @Override
+    public void DeinitialiseComponents() {
+
+    }
+}
--- src/SnakeMain.java
+++ src/SnakeMain.java
@@ -1,5 +1,20 @@
+import engine.Engine;
+import engine.systems.GameObjectHandlerSystem;
+
 public class SnakeMain {
     public static void main(String args[]) {
-        new Game(new SnakeWindowProperties());
+        //Initialise Engine
+        Engine.Get();
+        //Initialise Engine Systems
+        Engine.Get().AddSystem(new GameObjectHandlerSystem());
+
+        //Initialise game
+        SnakeGame snakeGame = new SnakeGame(new SnakeWindowProperties());
+        //Initialise Game Systems
+        snakeGame.InitialiseSystems();
+        //Initialise Game Components
+        snakeGame.InitialiseComponents();
+
+        snakeGame.Start();
     }
 }
